fa43238ed5384211d0386c0d54ee24fbc3cabb4e
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-02-03 14:11:19 +0000

    add pcChoice random pick function

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// Load DOM and add event listener to buttons
+
+
+
+
+// pcChoice calculation option
+
+let pcChoice = Math.floor(Math.random() * 5) + 1;
+
+if (pcChoice === 1){
+    pcChoice = "Rock";
+}else if(pcChoice === 2){
+    pcChoice = "Paper";
+}else if(pcChoice === 3){
+    pcChoice = "Scissors";
+}else if(pcChoice === 4){
+    pcChoice = "Lizard";
+}else if(pcChoice === 5){
+    pcChoice = "Spock";
+}
+
+
+console.log(pcChoice);
+
+
+
+// Used to check answers of who won 
+
+
+// Correct answer incrementation
+
+// Incorrect answer incrementation
+
+
+// Message on how the game was won
